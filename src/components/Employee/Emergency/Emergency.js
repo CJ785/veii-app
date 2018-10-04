7f6f2bc7512c04ba1bff42e78045baa884f16373
CJ785 <jayway777@gmail.com>
2018-10-03 21:14:22 -0400

    update

---
+++ src/components/Employee/Emergency/Emergency.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Emergency.css";
+import Select from 'react-select'
+import axios from 'axios';
+
+export default class Emergency extends React.PureComponent {
+
+    handleClick(e) {
+        // e.preventDefault();
+        console.log('front end text log');
+
+        axios.post('/emergency', {
+
+        });
+    }
+
+    render() {
+        return (
+            <div onClick={() => this.handleClick()}>Emergency</div>
+        )
+    }
+}
+
