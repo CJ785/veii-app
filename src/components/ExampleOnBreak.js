48f8554f37948b685830e287088e6eb27aecd03c
stevemazanec <stevenedwardmazanec@gmail.com>
2018-10-08 21:03:55 -0400

    capitalize

--- src/components/ExampleOnBreak.js
+++ src/components/ExampleOnBreak.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import OnBreakList from "./Manager/OnBreak/onbreakList"
+import OnBreakList from "./Manager/OnBreak/OnBreakList"
 import Modal from 'react-responsive-modal';
 
 
